d8ad0d02b76617f1dd52a4b85290b01415fb19dc
jonathanscosta <costajonathan@gmail.com>
2024-11-20 20:38:33 -0300

    prova da aula 01 LP

---
+++ provaAula01.py
@@ -0,0 +1,6 @@
+base = int(input(' qual medida da base do triângulo?    '))
+altura = int (input(' qual a medida da altura do triângulo?  '))
+
+area = base * altura /2
+
+print(f'a área do triângulo é {area:.1f}')
