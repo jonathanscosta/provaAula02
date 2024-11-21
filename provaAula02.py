65111a15e71c2fa118865b23c7ab5fa9dd0d21c2
jonathanscosta <costajonathan@gmail.com>
2024-11-20 21:01:08 -0300

    prova da segunda aula de LP

---
+++ provaAula02.py
@@ -0,0 +1,8 @@
+numero = float(input(' digite seu número   '))
+
+if(numero > 0):
+    print('seu número é positivo')
+elif (numero < 0 ):
+    print (' seu número é negativo')
+else:
+    print (' seu nuúmero é zero')
